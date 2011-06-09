67a1a611a070415d7acd6703478501cddaa34409
Antonio Cuni <anto.cuni@gmail.com>
2011-06-09 10:18:41 +0200

    don't use setMinimumSize, else we cannot resize the window after it's created; also, 1300px is the minimum to fully display the left menu

--- bin/qwebview.py
+++ bin/qwebview.py
@@ -18,7 +18,7 @@ def main():
 
     app = QApplication(sys.argv)
     web = QWebView()
-    web.setMinimumSize(1024, 800)
+    web.resize(1300, 1000)
     web.setWindowTitle(title)
     web.load(QUrl(url))
     web.show()
