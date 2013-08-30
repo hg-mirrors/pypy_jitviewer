53a2301e6eabd7dcbd63e3adee72f83f226ab61c
Edd Barrett <vext01@gmail.com>
2013-08-30 16:19:42 +0100

    (RichardN, Edd) Add the jitviewer path to PYTHONPATH automatically.

--- bin/jitviewer.py
+++ bin/jitviewer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env pypy
 import sys
+import os.path
+
+script_path = os.path.abspath(__file__)
+pythonpath = os.path.dirname(os.path.dirname(script_path))
+sys.path.append(pythonpath)
+
 from _jitviewer.app import main
 main(sys.argv)
