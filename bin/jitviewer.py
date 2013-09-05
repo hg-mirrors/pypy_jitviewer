afdd5a8e71d89352a59c5451fe2e9620b3c5a7d3
Edd Barrett <vext01@gmail.com>
2013-09-05 13:12:39 +0100

    Check that we are running under PyPy.
    "< arigato> for example I think that jitviewer doesn't work on CPython"

--- bin/jitviewer.py
+++ bin/jitviewer.py
@@ -6,5 +6,10 @@ script_path = os.path.abspath(__file__)
 pythonpath = os.path.dirname(os.path.dirname(script_path))
 sys.path.append(pythonpath)
 
+# Check we are running with PyPy
+if "pypy" not in os.path.basename(sys.executable):
+    print("error: jitviewer must be run with PyPy")
+    sys.exit(1)
+
 from _jitviewer.app import main
 main(sys.argv)
